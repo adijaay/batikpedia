d193482c8ab69169d8e8e84ca290be8167c103ec
adijaay <adiwijayaswastama@gmail.com>
2024-06-03 11:19:50 +0700

    change profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,35 +3,42 @@ import { AuthContext } from "../Provider/Auth";
 import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
-    //ini manggil variabel data isLoggedIn pake useContext yang buat ngecek udah login apa belum dari AuthContext
-    const {isLoggedIn}= useContext(AuthContext);
-    //ini menginisiasi navigasi pake useNavigate biar bisa pindah pindah halaman
-    const navigate = useNavigate();
-    
-    /*
+  //ini manggil variabel data isLoggedIn pake useContext yang buat ngecek udah login apa belum dari AuthContext
+  const { isLoggedIn } = useContext(AuthContext);
+  //ini menginisiasi navigasi pake useNavigate biar bisa pindah pindah halaman
+  const navigate = useNavigate();
+
+  /*
     Ini useEffect biar jalan pas halaman Home diload.
     fungsi dalemnya buat ngasih perintah dimana kalo user belum login maka dikembalikan ke halaman login.
     */
-    useEffect(() => {
-        if(!isLoggedIn){
-            navigate("/login")
-        }
-    }, [isLoggedIn])
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoggedIn]);
 
-    //ini return halaman yang bakal tampil ke website saat route "/profile" atau di About
-    return(
-        <div className="flex flex-col gap-4">
-            <h1 className="uppercase tracking-wide text-5xl text-white font-bold text-left">About</h1>
-            <h1 className="uppercase tracking-wide text-2xl text-gray-400 font-regular text-left">BATIKPEDIA</h1>
-            <div className="border border-gray-300 rounded-xl p-4 text-justify">
-                <p>BATIKPEDIA ini dibuat oleh Kelompok 11 yang beranggotakan 4 anggota, yaitu 
-                <span className="uppercase tracking-wide text-2xl text-blue-400 font-regular text-left"> ROY PUTRA RIONA NADHIIER, </span>
-                <span className="uppercase tracking-wide text-2xl text-blue-400 font-regular text-left">AHMAD ALDANI HERLANGGA, </span>
-                <span className="uppercase tracking-wide text-2xl text-blue-400 font-regular text-left">IMAM NURALIM, </span>
-                <span className="uppercase tracking-wide text-2xl text-blue-400 font-regular text-left">DIEFA EDRA RIVANDI. </span>
-                Aplikasi ini ditujukan sebagai tempat informasi batik di Indonesia yang dapat dibuka oleh khalayak umum, sehingga dapat mempermudah mempelajari informasi batik. 
-                </p>
-            </div>
-        </div>
-    )
+  //ini return halaman yang bakal tampil ke website saat route "/profile" atau di About
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="uppercase tracking-wide text-5xl text-white font-bold text-left">
+        About
+      </h1>
+      <h1 className="uppercase tracking-wide text-2xl text-gray-400 font-regular text-left">
+        BATIKPEDIA
+      </h1>
+      <div className="border border-gray-300 rounded-xl p-4 text-justify">
+        <p>
+          BATIKPEDIA ini dibuat oleh
+          <span className="uppercase tracking-wide text-2xl text-blue-400 font-regular text-left">
+            {" "}
+            ADI WIJAYA SWASTAMA{" "}
+          </span>
+          Aplikasi ini ditujukan sebagai tempat informasi batik di Indonesia
+          yang dapat dibuka oleh khalayak umum, sehingga dapat mempermudah
+          mempelajari informasi batik.
+        </p>
+      </div>
+    </div>
+  );
 }
